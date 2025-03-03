91ccb1ee8647bc9bc2c5fddaf41dd614c70ab221
root <itsky5000@gmail.com>
2025-03-02 22:46:26 -0600

    Dashboard Update
    Colors and reformatting

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -26,14 +26,54 @@ return {
             config = {
                 header = header,
                 shortcut = {
-                    { icon = '󰋖 ', desc = 'Help', action = 'Telescope help_tags', key = 'h' },
-                    { icon = ' ', desc = 'New File', action = 'enew', key = 'n' },
-                    { icon = '󰱼 ', desc = 'Find File', action = 'Telescope find_files', key = 'f' },
-                    { icon = '󱎸 ', desc = 'Search', action = 'Telescope live_grep', key = 's' },
-                    { icon = '󰙅 ', desc = 'File Tree', action = 'Neotree filesystem reveal float', key = 't' },
-                    { icon = '󰊢 ', desc = 'Git GUI', action = 'LazyGit', key = 'g' },
-                    { icon = '󰚰 ', desc = 'Lazy Update', action = 'Lazy update', key = 'L' },
-                    { icon = '󰚰 ', desc = 'Mason Update', action = 'MasonUpdate', key = 'M' },
+                    {
+                        icon = '󰋖 ', desc = 'Help',
+                        group = 'DiagnosticFloatingError',
+                        action = 'Telescope help_tags',
+                        key = 'h'
+                    }, {
+                        icon = ' ', desc = 'New File',
+                        group = 'DiagnosticFloatingWarn',
+                        action = 'enew',
+                        key = 'n'
+                    }, {
+                        icon = '󰱼 ', desc = 'Find File',
+                        group = 'DiagnosticFloatingHint',
+                        action = 'Telescope find_files',
+                        key = 'f'
+                    }, {
+                        icon = '󱎸 ', desc = 'Search',
+                        group = 'DiagnosticFloatingOk',
+                        action = 'Telescope live_grep',
+                        key = 's'
+                    }, {
+                        icon = '󰙅 ', desc = 'File Tree',
+                        action = 'Neotree filesystem reveal float',
+                        key = 't'
+                    }, {
+                        icon = '󰊢 ', desc = 'Git GUI',
+                        group = '@variable',
+                        action = 'LazyGit',
+                        key = 'G'
+                    }, {
+                        icon = '󰤄 ', desc = 'Lazy GUI',
+                        group = '@module.builtin',
+                        action = 'Lazy',
+                        key = 'L'
+                    }, {
+                        icon = '󱁤 ', desc = 'Mason GUI',
+                        group = '@function',
+                        action = 'Mason',
+                        key = 'M'
+                    }, {
+                        icon = '󰚰 ', desc = 'Update Plugins',
+                        group = '@attribute',
+                        action = function()
+                            vim.cmd('Lazy update')
+                            vim.cmd('MasonUpdate')
+                        end,
+                        key = 'U'
+                    },
                 },
                 project = {
                     enable = true,
